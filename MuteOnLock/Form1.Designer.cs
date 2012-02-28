86535513a33cd3f12cbd21c96d071063d20d77d9
unknown <s.turlo@.itransition.corp>
2012-02-28 12:55:07 +0300

    initial commit

---
+++ MuteOnLock/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace MuteOnLock
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.runOnStartUp = new System.Windows.Forms.ToolStripMenuItem();
+            this.exit = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.runOnStartUp,
+            this.exit});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(153, 48);
+            // 
+            // runOnStartUp
+            // 
+            this.runOnStartUp.Name = "runOnStartUp";
+            this.runOnStartUp.Size = new System.Drawing.Size(152, 22);
+            this.runOnStartUp.Text = "Run on startup";
+            this.runOnStartUp.Click += new System.EventHandler(this.RunOnStartUp_Click);
+            // 
+            // exit
+            // 
+            this.exit.Name = "exit";
+            this.exit.Size = new System.Drawing.Size(152, 22);
+            this.exit.Text = "Exit";
+            this.exit.Click += new System.EventHandler(this.Exit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(116, 0);
+            this.Name = "Form1";
+            this.Text = "Mute On Lock";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem runOnStartUp;
+        private System.Windows.Forms.ToolStripMenuItem exit;
+    }
+}
+
---
+++ MuteOnLock/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using Microsoft.Win32;
+
+namespace MuteOnLock
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("user32.dll")]
+        public static extern IntPtr SendMessageW(IntPtr hWnd, int Msg, IntPtr wParam, IntPtr lParam);
+
+        private const int APPCOMMAND_VOLUME_MUTE = 0x80000;
+        private const int WM_APPCOMMAND = 0x319;
+        private bool runOnStartUp_;
+
+        public Form1()
+        {
+            InitializeComponent();
+            SystemEvents.SessionSwitch += new SessionSwitchEventHandler(SystemEvents_SessionSwitch);
+            this.ShowInTaskbar = false;
+            notifyIcon1.Visible = true;
+            notifyIcon1.Text = "Mute On Lock";
+            notifyIcon1.ContextMenuStrip = contextMenuStrip1;
+            runOnStartUp_ = Registry.CurrentUser.OpenSubKey(@"Software\Microsoft\Windows\CurrentVersion\Run").GetValue(Application.ProductName) != null;
+            runOnStartUp.Checked = runOnStartUp_;
+        }
+
+        private void SystemEvents_SessionSwitch(object sender, Microsoft.Win32.SessionSwitchEventArgs e)
+        {
+            if (e.Reason == SessionSwitchReason.SessionLock || e.Reason == SessionSwitchReason.SessionUnlock)
+            {
+                SendMessageW(this.Handle, WM_APPCOMMAND, this.Handle, (IntPtr)APPCOMMAND_VOLUME_MUTE);
+            }
+        }
+
+        private void RunOnStartUp_Click(object sender, EventArgs e)
+        {
+            RegistryKey runKey = Registry.CurrentUser.OpenSubKey(@"Software\Microsoft\Windows\CurrentVersion\Run", true);
+
+            if (runOnStartUp_)
+            {
+                runKey.DeleteValue(Application.ProductName);
+                runOnStartUp_ = false;
+
+            }
+            else
+            {
+                runKey.SetValue(Application.ProductName, Application.ExecutablePath);
+                runOnStartUp_ = true;
+            }
+            runOnStartUp.Checked = runOnStartUp_;
+        }
+
+        private void Exit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+           Hide();
+        }
+
+    }
+    
+}
